7f83b616d1c1ece865369cbb2e03d882393b545a
Ashish Sharma <sharmaashish049@gmail.com>
2022-11-27 11:39:41 +0530

    Connection string added

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
-const uri = process.env.MONGO_URI
-mongoose.connect(uri, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(data => {
     if (data) {
